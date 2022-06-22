ef917a74efe9a68891b2f8201eb1573fe3725265
Noraisn <noraiseni6972@gmail.com>
2022-06-23 02:05:49 +0300

    y

--- script.js
+++ script.js
@@ -31,6 +31,7 @@ let pconf = document.querySelector('.box-result p')
         async function initialize() {
             let status = document.querySelector('.init_status')
             status.innerHTML = 'Loading Model .... <span class="fa fa-spinner fa-spin"></span>'
+            console.log('modelNOTloaded')
             const model = await tf.loadLayersModel('./tensorflowjs-model2/model.json');
             console.log('modelLoaded', model)
             status.innerHTML = 'Model Loaded Successfully  <span class="fa fa-check"></span>'
